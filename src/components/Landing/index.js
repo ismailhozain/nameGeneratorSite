b542a05aae6658e4d66eee93dfac2815e7a9bf3b
Ismail Hozain <ismailhozain@gmail.com>
2020-05-02 14:19:32 -0500

    Update index.js

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 const Landing = () => (
   <div className={"titleDiv"}>
     <h1 className={"title"}>Your app goes here!</h1>
-      <Button>This is a button</Button>
   </div>
 );
 
