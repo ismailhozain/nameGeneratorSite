589e03234cdf779ec42efd57a969d1826083f376
Ismail Hozain <ismailhozain@gmail.com>
2020-05-03 11:58:00 -0500

    updated package.json to fix issue with momentjs update.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import {Menu} from "antd";
 import 'antd/dist/antd.css';
-import Logo from './logo.png';
 import './navigation.css';
 const NavigationWrapper = () => (
     <div>
@@ -14,7 +13,6 @@ const NavigationWrapper = () => (
 const Navigation = () => (
     <Menu mode={"horizontal"}>
          <Menu.Item>
-             <Link to={ROUTES.LANDING}><img alt = "the logo of this site. This is the logo of Ismail Hozain, circa 2018, a black dragon surrounding his initials I and H" src={Logo}/></Link>
         </Menu.Item>
     </Menu>
 );
