11aeaeb57e232d59eef82552caaa5226ee8d4114
Ismail Hozain <ismailhozain@gmail.com>
2020-05-15 14:29:42 -0500

    Bound functions to three different algorithms, also deleted algorithm.js and will begin porting the algorithm from java to js.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,35 +1,84 @@
 import React from 'react';
 import './landing.css';
 import { Switch, Card, Button } from 'antd';
-
 import styled from 'styled-components';
 
 
-import Output from "./algorithm";
+
+
 class Toggling extends React.Component {
-    state = {
-        disabled: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            disabled: false,
+            genderState: true,
+
+        };
+        this.generate = this.generate.bind(this);
+    }
+
+    generate = () => {
+        console.table(this.state);
+        if(this.state.disabled) {
+            AlgorithmAll();
+        }
+        else{
+            if(this.state.genderState) {
+                AlgorithmMale();
+            }
+            else{
+                AlgorithmFemale();
+            }
+        }
+    };
+
+    gender = () => {
+        this.setState({
+            genderState: !this.state.genderState,
+        })
     };
 
     toggle = () => {
         this.setState({
             disabled: !this.state.disabled,
+
         });
     };
 
     render() {
         return (
             <div>
-                <Switch disabled={this.state.disabled} defaultChecked={"checked"} checkedChildren={"Male Name"} unCheckedChildren={"Female Name"}>Male</Switch>
+                <Switch disabled={this.state.disabled} onClick={this.gender} defaultChecked={"checked"} checkedChildren={"Male Name"} unCheckedChildren={"Female Name"}>Male</Switch>
                 <br />
                 <Button type="danger" onClick={this.toggle}>
                     All names
                 </Button>
+                <Button type={"primary"} onClick={this.generate}>Generate</Button>
             </div>
         );
     }
 }
 
+function AlgorithmAll() {
+    alert("all");
+}
+
+function AlgorithmMale() {
+    alert("male");
+}
+function AlgorithmFemale() {
+    alert("female");
+}
+
+class Output extends React.Component {
+    render() {
+        return(
+          <div>
+              <h1>This is great output!</h1>
+          </div>
+        );
+    }
+}
 const Landing = () => (
     <div className={"all"}>
         <div className={"titleDiv"}>
@@ -38,10 +87,9 @@ const Landing = () => (
         <div className={"content"}>
             <Card title={"Configure Output"}>
                 <Toggling/>
-                <Button type={"primary"}>Generate</Button>
             </Card>
             <Card title={"Output"}>
-                <Output/>
+                    <Output/>
             </Card>
         </div>
     </div>
